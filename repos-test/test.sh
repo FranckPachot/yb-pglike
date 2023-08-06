e433c5af087049a87f03c9584221e8d6da5dd92f
Franck Pachot <mail@pachot.net>
2023-08-06 18:21:49 +0200

    add some repo tests

--- repos-test/test.sh
+++ repos-test/test.sh
@@ -1,76 +1,39 @@
 
 img="https://github.com/FranckPachot/yb-pglike.git"
-mkdir -p /var/tmp/tests 
+mkdir -p /var/tmp/repos-tests 
 
-# Search on GitHub:
+# Search repos on GitHub:
 #
 # path:/docker-compose* /image: postgres/ /POSTGRES_PASSWORD:/ language:YAML  NOT is:archived 
 #
 
-cd       /var/tmp/tests &&
-rm -rf shiori
-git clone https://github.com/choboellobo/odoo-16.git
-cd ./odoo-16 &&
-sed -e 's?image: postgres.*?build: '"${img}"'?' -i docker-compose.yml &&
-cat docker-compose.yml &&
-docker-compose -f docker-compose.yml up
+case $1 in 
 
-exit
+aws-dataall) 
 
-cd       /var/tmp/tests &&
-rm -rf shiori
-git clone https://github.com/go-shiori/shiori.git
-cd ./shiori &&
-sed -e 's?image: postgres.*?build: '"${img}"'?' -i docker-compose.yaml &&
-sed -e 's/SHIORI_DBMS: mysql/SHIORI_DBMS: postgres/' -i docker-compose.yaml &&
-cat docker-compose.yaml &&
-docker-compose -f docker-compose.yaml up
-
-exit
-
-cd       /var/tmp/tests &&
-git clone https://github.com/lucasavila00/papercups.git
-cd ./papercups &&
-sed -e 's!image: postgres!build: '"${img}"'!' -i docker-compose.dev.yml &&
-docker-compose -f docker-compose.dev.yml up
-
-exit
-
-cd       /var/tmp/tests &&
-git clone https://github.com/meienberger/runtipi.git
-cd ./runtipi &&
-#sed -e 's!image: postgres!build: '"${img}"'!' -i docker-compose.yml &&
-#cat .env.example .env.test > .env && docker-compose -f docker-compose.yml up
-docker-compose -f docker-compose.test.yml up
-
-exit
-
-cd       /var/tmp/tests &&
-git clone https://github.com/photoprism/photoprism.git
-cd ./photoprism &&
-sed -e 's!image: postgres!build: '"${img}"'!' -i docker-compose.postgres.yml &&
-docker-compose -f docker-compose.postgres.yml up
-
-exit
-
-cd       /var/tmp/tests &&
-git clone https://github.com/initOS/dob.git
-cd ./dob &&
-#sed -e 's!image: postgres!build: '"${img}"'!' -i docker-compose.postgres.yml &&
-./setup.sh && docker-compose up
-
-exit
-
-########### Following works
-
-
-# A modern data marketplace that makes collaboration among diverse users (like business, analysts and engineers) easier, increasing efficiency and agility in data projects on AWS.
-cd       /var/tmp/tests &&
+cd       /var/tmp/repos-tests &&
 git clone https://github.com/awslabs/aws-dataall 
 cd ./aws-dataall &&
 sed -e 's!context: compose/postgres!context: '"${img}"'!' -i docker-compose.yaml &&
-docker-compose up
+docker-compose -p aws-dataall up
 
+;;
 
-exit
+wiki) 
+
+cd       /var/tmp/repos-tests &&
+git clone https://github.com/requarks/wiki
+cd ./wiki/dev/examples &&
+sed -e 's!image: postgres.*!build: '"${img}"'!' -i docker-compose.yml &&
+docker-compose -p wiki up
+
+;;
+
+*)
+
+grep -A1 "^[^)*]*[)]$" $0
+
+;;
+
+esac
 
