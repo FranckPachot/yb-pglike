643f486667e016152a705afe3a1a0e82c7a02ad1
Franck Pachot (YB) <mail@pachot.net>
2023-08-06 10:48:58 +0000

    add auto push

--- simple-tests/test-and-push.sh
+++ simple-tests/test-and-push.sh
@@ -1,13 +1,8 @@
 docker compose -f  docker-compose-context.yaml down
 docker compose -f  docker-compose-context.yaml up -d
-until
-docker compose -f  docker-compose-context.yaml logs app | grep -C 100 " done in"
-do
-sleep 1
-done
-until
-docker compose -f docker-compose-context.yaml run app pgbench -nh db 
-do
-sleep 1
-done
-( cd .. && docker build -t pachot/yb-pglike . && docker push pachot/yb-pglike )
+docker compose -f  docker-compose-context.yaml logs -f app
+read -p  "Do you want to push the image to Docker Hub? " rep
+echo "$rep" | grep -i ^y && {
+ docker login -u pachot
+ cd .. && docker build -t pachot/yb-pglike . && docker push pachot/yb-pglike 
+}
