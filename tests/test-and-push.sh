e77a251f911809d8c50d0fc50b61d9f606915ed8
Franck Pachot <mail@pachot.net>
2023-08-06 22:36:53 +0200

    remove image to be sure to build it

--- tests/test-and-push.sh
+++ tests/test-and-push.sh
@@ -1,4 +1,6 @@
+cd $(dirname $0)
 docker compose -f  docker-compose-context.yaml down
+docker image rm $(basename $PWD)-db
 docker compose -f  docker-compose-context.yaml up -d
 docker compose -f  docker-compose-context.yaml logs -f app
 read -p  "Do you want to push the image to Docker Hub? " rep
